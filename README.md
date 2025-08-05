791c6b894a1806c5a0ba0498ddc8340765c6851e
Ana Beatriz Monteiro <abeatrizbmonteiro@gmail.com>
2025-08-05 23:51:05 +0000

    Primeiro programa com print

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # bfd_aula_noite
+aula 01 - documentação em https://peps.python.org
+
+limpar o terminal com um espaço: ctrl + L; limpar completo: clear + enter
---
+++ aula01/meu_primeiro_programa.py
@@ -0,0 +1,12 @@
+print("Olá mundo em .py!");
+print("Olá mundo em .py!","Eu sou Beatriz");
+ #tamanho infinito (?); além da vírgula, posso usar o +, mas ele não dá espaço do mesmo jeito, 
+ # faz apena concatenação
+
+print("Olá mundo em .py!"+"Eu sou Beatriz");
+
+print("Olá mundo em .py!", "Eu sou Beatriz", sep="*");
+
+print("Olá mundo! ", end = " " "Eu sou Beatriz ") # " " para indicar o espaço antes da nova string
+
+print("Olá mundo em .py! \n Eu sou Beatriz") #\n como quebra de linha
