9819831db0dc4e53f1ee51ac0ccd931b3168a7d2
Beatriz Monteiro <100790246+abeamonteiro@users.noreply.github.com>
2025-09-04 22:19:26 +0000

    resolvendo conflito

--- exercícios/exercícios_funções.py
+++ exercícios/exercícios_funções.py
@@ -20,6 +20,8 @@ print("O dobro do número indicado é", (dobro(num1)))
 # 3. Crie uma função chamada `soma` que receba dois números e retorne a soma deles. 
 # Depois, use a função para somar **10 + 20**.
 
+numb1 = int(input(""))
+
 
 # 4. Crie uma função chamada `mensagem` que receba um nome como parâmetro e exiba a mensagem:
 #    "Olá, \[nome]!"
