f484be6b95800a2739913d50050fa9974b4b9026
Beatriz Monteiro <100790246+abeamonteiro@users.noreply.github.com>
2025-10-23 23:01:42 +0000

    adiciona bd escola

---
+++ exercícios/poo/poo_relacao_classes.py
@@ -0,0 +1,12 @@
+#1. Crie as classes `Pessoa` e `Livro` e demonstre uma relação de associação entre eles.
+class Pessoa:
+    
+#2. Crie as classes `Aluno` e `Onibus`. Crie um método em `Aluno` que use a classe `Onibus`.
+
+#3. Crie uma classe `Funcionario` e `Departamento` que contém uma lista de `Funcionarios`.`Departamento` deve agregar funcionários já criados.
+
+#4. Crie as classes `Time` e `Jogador`. Um `Jogador` tem nome e posição como atributos, enquanto `Time` agrega jogadores em uma lista.
+
+#5. Crie a classe `Carro` que possui um `Motor`. O `Motor` deve ser criado dentro do `Carro`. Se o `Carro` deixar de existir, o `Motor` também deixa. Mostre isso criando e depois apagando o objeto.
+
+#6. Crie a classe `Casa` que é composta por vários `Comodo`s (sala, cozinha, quarto, etc.). Cada `Comodo` deve ser criado dentro da `Casa`.
